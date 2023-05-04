fc8079c4f34e9ca1eaaa99129292df0641563de6
Peter Quinn <peterdquinnsr@gmail.com>
2023-05-04 16:11:48 -0700

    pwa finished

--- js/sw.js
+++ js/sw.js
@@ -1,22 +1,56 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('casino-app-cache').then((cache) => {
-        return cache.addAll([
-          '/index.html',
-          '/manifest.json',
-          '/css/styles.css',
-          '/js/main.js',
-          // Add all other necessary files for your app here
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = "casino-app-cache-v1";
+const assetsToCache = [
+  "/",
+  "index.html",
+  "css/styles.css",
+  "js/main.js",
+  // Add the paths to other HTML, CSS, JS, and image files for your app
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Removing old cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
