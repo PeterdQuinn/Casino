7df50ae206216dcfa2bdd1dfec5f9817f79c61ba
Peter Quinn <peterdquinnsr@gmail.com>
2023-05-05 16:50:52 -0700

    edit first 2 games pages

--- js/game1.js
+++ js/game1.js
@@ -2,9 +2,38 @@ const higherBtn = document.getElementById("higher-btn");
 const lowerBtn = document.getElementById("lower-btn");
 const currentNumber = document.getElementById("current-number");
 const result = document.getElementById("result");
+const bet1Btn = document.getElementById("bet-1");
+const bet5Btn = document.getElementById("bet-5");
+const bet10Btn = document.getElementById("bet-10");
 
 let currentNum = generateRandomNumber();
 currentNumber.textContent = currentNum;
+
+let currentBet = 1;
+
+bet1Btn.classList.add("active");
+
+bet1Btn.addEventListener("click", () => {
+  currentBet = 1;
+  setActiveBetButton(bet1Btn);
+});
+
+bet5Btn.addEventListener("click", () => {
+  currentBet = 5;
+  setActiveBetButton(bet5Btn);
+});
+
+bet10Btn.addEventListener("click", () => {
+  currentBet = 10;
+  setActiveBetButton(bet10Btn);
+});
+
+function setActiveBetButton(button) {
+  bet1Btn.classList.remove("active");
+  bet5Btn.classList.remove("active");
+  bet10Btn.classList.remove("active");
+  button.classList.add("active");
+}
 
 higherBtn.addEventListener("click", () => {
   playGame(true);
@@ -15,13 +44,19 @@ lowerBtn.addEventListener("click", () => {
 });
 
 function playGame(isHigher) {
+  if (currentBet <= 0) {
+    result.textContent = "Please place a bet to play.";
+    return;
+  }
+
   const newNumber = generateRandomNumber();
   const isCorrect = (isHigher && newNumber > currentNum) || (!isHigher && newNumber < currentNum);
 
   currentNum = newNumber;
   currentNumber.textContent = currentNum;
   
-  result.textContent = isCorrect ? "You guessed correctly!" : "Oops! Wrong guess!";
+  result.textContent = isCorrect ? `You guessed correctly! You won $${currentBet}.` : "Oops! Wrong guess!";
+  currentBet = isCorrect ? currentBet : currentBet * -1;
 }
 
 function generateRandomNumber() {
--- js/game2.js
+++ js/game2.js
@@ -1,14 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const triggerBtn = document.getElementById("trigger-btn");
-  const result = document.getElementById("result");
-
-  triggerBtn.addEventListener("click", function() {
-      const randomNumber = Math.random();
-
-      if (randomNumber < 0.1666) {
-          result.textContent = "Bang! Your time to go!";
-      } else {
-          result.textContent = "Click! Not your time amigo!";
-      }
+    const triggerBtn = document.getElementById("trigger-btn");
+    const result = document.getElementById("result");
+  
+    triggerBtn.addEventListener("click", function() {
+        const randomNumber = Math.random();
+  
+        if (randomNumber < 0.1666) {
+            result.textContent = "Bang! Your time to go!";
+        } else {
+            result.textContent = "Click! Not your time amigo!";
+        }
+    });
+    
+    // Add event listeners to the bet buttons
+    const bet1Btn = document.getElementById("bet-1");
+    const bet5Btn = document.getElementById("bet-5");
+    const bet10Btn = document.getElementById("bet-10");
+    
+    bet1Btn.addEventListener("click", function() {
+      placeBet(1);
+    });
+    
+    bet5Btn.addEventListener("click", function() {
+      placeBet(5);
+    });
+    
+    bet10Btn.addEventListener("click", function() {
+      placeBet(10);
+    });
+    
+    // Function to place a bet
+    function placeBet(amount) {
+      console.log(`Placing a $${amount} bet`);
+      // Add your code to place the bet here
+    }
   });
-});
+  
