ad21c0ded41662db6c9b4bd3f219afa2ee00a1ec
Peter Quinn <peterdquinnsr@gmail.com>
2023-05-09 13:08:56 -0700

    dice roll working, needs layout

--- js/dice_rolling.js
+++ js/dice_rolling.js
@@ -1,15 +1,77 @@
-const rollBtn = document.getElementById('roll-btn');
-const dice1 = document.getElementById('dice1');
-const dice2 = document.getElementById('dice2');
+function createDice(number) {
+	const dotPositionMatrix = {
+		1: [
+			[50, 50]
+		],
+		2: [
+			[20, 20],
+			[80, 80]
+		],
+		3: [
+			[20, 20],
+			[50, 50],
+			[80, 80]
+		],
+		4: [
+			[20, 20],
+			[20, 80],
+			[80, 20],
+			[80, 80]
+		],
+		5: [
+			[20, 20],
+			[20, 80],
+			[50, 50],
+			[80, 20],
+			[80, 80]
+		],
+		6: [
+			[20, 20],
+			[20, 80],
+			[50, 20],
+			[50, 80],
+			[80, 20],
+			[80, 80]
+		]
+	};
 
-rollBtn.addEventListener('click', () => {
-  const dice1Value = getRandomDiceValue();
-  const dice2Value = getRandomDiceValue();
+	const dice = document.createElement("div");
 
-  dice1.textContent = dice1Value;
-  dice2.textContent = dice2Value;
-});
+	dice.classList.add("dice");
 
-function getRandomDiceValue() {
-  return Math.floor(Math.random() * 6) + 1;
+	for (const dotPosition of dotPositionMatrix[number]) {
+		const dot = document.createElement("div");
+
+		dot.classList.add("dice-dot");
+		dot.style.setProperty("--top", dotPosition[0] + "%");
+		dot.style.setProperty("--left", dotPosition[1] + "%");
+		dice.appendChild(dot);
+	}
+
+	return dice;
 }
+
+function randomizeDice(diceContainer, numberOfDice) {
+	diceContainer.innerHTML = "";
+
+	for (let i = 0; i < numberOfDice; i++) {
+		const random = Math.floor((Math.random() * 6) + 1);
+		const dice = createDice(random);
+
+		diceContainer.appendChild(dice);
+	}
+}
+
+const NUMBER_OF_DICE = 5;
+const diceContainer = document.querySelector(".dice-container");
+const btnRollDice = document.querySelector(".btn-roll-dice");
+
+randomizeDice(diceContainer, NUMBER_OF_DICE);
+
+btnRollDice.addEventListener("click", () => {
+	const interval = setInterval(() => {
+		randomizeDice(diceContainer, NUMBER_OF_DICE);
+	}, 50);
+
+	setTimeout(() => clearInterval(interval), 1000);
+});
